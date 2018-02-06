3f92ea12a5e59ee99c093eca3a277b22ab96557b
Jeffery "J.D." Kelly <neohero22@gmail.com>
2018-02-06 12:18:54 -0800

    Added in magnet tiles that function similarly to the gloves to allow for additional puzzle options and introduce the ideas of the Pushing/Pulling separately from the gloves themselves.

---
+++ PrincessCape/Assets/Scripts/MagnetTile.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagnetTile : ActivatedObject {
+    Animator myAnimator;
+    BoxCollider2D magField;
+    [SerializeField]
+    bool pull = true;
+    float force = 15f;
+    public override void Activate()
+    {
+        myAnimator.SetTrigger("Activate");
+        magField.enabled = true;
+    }
+
+    public override void Deactivate()
+    {
+        myAnimator.SetTrigger("Deactivate");
+        magField.enabled = false;
+    }
+
+    // Use this for initialization
+    void Awake () {
+        myAnimator = GetComponent<Animator>();
+        magField = GetComponent<BoxCollider2D>();
+        magField.enabled = false;
+	}
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Metal")) {
+            collision.attachedRigidbody.AddForce((pull ? -transform.up : transform.up) * force);
+        }
+    }
+}
