12e6844beefd37efd9148e920045952684763548
Jeffery "J.D." Kelly <neohero22@gmail.com>
2018-02-07 16:38:13 -0800

    Added in conveyor belts.

---
+++ PrincessCape/Assets/Scripts/ConveyorBelt.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConveyorBelt : ActivatedObject
+{
+    Animator myAnimator;
+    BoxCollider2D theBelt;
+    Vector2 pushDir = Vector2.right;
+    float pushForce = 5;
+    private void Awake()
+    {
+        myAnimator = GetComponent<Animator>();
+        theBelt = GetComponent<BoxCollider2D>();
+        theBelt.enabled = false;
+
+        if (GetComponent<SpriteRenderer>().flipX) {
+            pushDir *= -1;
+        }
+    }
+    public override void Activate()
+    {
+        myAnimator.SetTrigger("Activate");
+        theBelt.enabled = true;
+    }
+
+    public override void Deactivate()
+    {
+        myAnimator.SetTrigger("Deactivate");
+        theBelt.enabled = false;
+    }
+
+    public void OnTriggerStay2D(Collider2D collision)
+    {
+        collision.attachedRigidbody.AddForce(pushDir * pushForce);
+    }
+}
