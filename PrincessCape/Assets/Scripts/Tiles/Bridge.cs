d0ab57a6e42391ae0d6ee10a0f9b883a9afd83bb
Jeffery "J.D." Kelly <neohero22@gmail.com>
2018-02-15 09:16:55 -0800

    Stairs and bridges now scale properly on being loaded from a PCL file.

--- PrincessCape/Assets/Scripts/Tiles/Bridge.cs
+++ PrincessCape/Assets/Scripts/Tiles/Bridge.cs
@@ -9,6 +9,8 @@ public class Bridge : ActivatedObject
     GameObject bridgeTile;
     [SerializeField]
     bool startActive = false;
+    [SerializeField]
+    Vector3 dir = Vector3.right;
 
     private void Start()
     {
@@ -54,14 +56,46 @@ public class Bridge : ActivatedObject
     {
         if (right)
         {
-            GameObject tile = Instantiate(bridgeTile);
-            tile.transform.SetParent(transform);
-            tile.transform.localPosition = new Vector3(transform.childCount, -0.5f, 0);
+            SpawnChild();
         }
         else if (transform.childCount > 0)
         {
             DestroyImmediate(transform.GetChild(transform.childCount - 1).gameObject);
         }
     }
+
+    void SpawnChild() {
+		GameObject tile = Instantiate(bridgeTile);
+		tile.transform.SetParent(transform);
+        tile.transform.localPosition = dir * transform.childCount + Vector3.down * 0.5f;
+    }
+
+    public override string SaveData()
+    {
+		if (transform.parent.name != "Map")
+		{
+			return "";
+		}
+		string info = "{\n";
+		info += string.Format("\"Name\": \"{0}\"", name.Split('(')[0]) + lineEnding;
+		info += string.Format("\"ID\": \"{0}\"", ID) + lineEnding;
+		info += string.Format("\"Position\": \"{0}\"", transform.position) + lineEnding;
+		info += string.Format("\"Rotation\": \"{0}\"", transform.rotation) + lineEnding;
+		info += string.Format("\"Scale\": \"{0}\"", transform.localScale) + lineEnding;
+		info += string.Format("\"Children\": \"{0}\"", transform.childCount) + lineEnding;
+		info += "}" + lineEnding;
+		return info;
+    }
+
+	public override void FromData(TileStruct tile)
+	{
+		base.FromData(tile);
+		int numChildren = PCLParser.ParseInt(tile.info[3]);
+
+		for (int i = 0; i < numChildren; i++)
+		{
+			SpawnChild();
+		}
+	}
 #endif
 }
--- PrincessCape/Assets/Scripts/Tiles/Stairs.cs
+++ PrincessCape/Assets/Scripts/Tiles/Stairs.cs
@@ -8,12 +8,43 @@ public class Stairs : MapTile {
 	public override void ScaleX(bool right)
     {
         if (right) {
-            GameObject child = Instantiate(stair);
-            child.transform.SetParent(transform);
-            child.transform.localScale = stair.transform.localScale.SetY(1 + transform.childCount / 2.0f);
-            child.transform.localPosition = transform.right * transform.childCount + (transform.up * (transform.childCount) / 4.0f);
+            SpawnChild();
         } else if (transform.childCount > 0) {
             DestroyImmediate(transform.GetChild(transform.childCount - 1).gameObject, false);
         }
+    }
+
+    public override string SaveData()
+    {
+		if (transform.parent.name != "Map")
+		{
+			return "";
+		}
+		string info = "{\n";
+		info += string.Format("\"Name\": \"{0}\"", name.Split('(')[0]) + lineEnding;
+		info += string.Format("\"ID\": \"{0}\"", ID) + lineEnding;
+		info += string.Format("\"Position\": \"{0}\"", transform.position) + lineEnding;
+		info += string.Format("\"Rotation\": \"{0}\"", transform.rotation) + lineEnding;
+		info += string.Format("\"Scale\": \"{0}\"", transform.localScale) + lineEnding;
+        info += string.Format("\"Children\": \"{0}\"", transform.childCount) + lineEnding;
+		info += "}" + lineEnding;
+		return info;
+    }
+
+    public override void FromData(TileStruct tile)
+    {
+        base.FromData(tile);
+        int numChildren = PCLParser.ParseInt(tile.info[3]);
+
+        for (int i = 0; i < numChildren; i++) {
+            SpawnChild();
+        }
+    }
+
+    void SpawnChild() {
+		GameObject child = Instantiate(stair);
+		child.transform.SetParent(transform);
+		child.transform.localScale = stair.transform.localScale.SetY(1 + transform.childCount / 2.0f);
+		child.transform.localPosition = transform.right * transform.childCount + (transform.up * (transform.childCount) / 4.0f);
     }
 }
