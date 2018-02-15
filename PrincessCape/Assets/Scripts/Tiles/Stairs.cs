b249c54ce62477924e4abecac66d954d87a488e6
Jeffery "J.D." Kelly <neohero22@gmail.com>
2018-02-15 09:00:27 -0800

    Created a Tile for stairs which spawns taller stairs the more its x scale increases.

---
+++ PrincessCape/Assets/Scripts/Tiles/Stairs.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stairs : MapTile {
+    [SerializeField]
+    GameObject stair;
+	public override void ScaleX(bool right)
+    {
+        if (right) {
+            GameObject child = Instantiate(stair);
+            child.transform.SetParent(transform);
+            child.transform.localScale = stair.transform.localScale.SetY(1 + transform.childCount / 2.0f);
+            child.transform.localPosition = transform.right * transform.childCount + (transform.up * (transform.childCount) / 4.0f);
+        } else if (transform.childCount > 0) {
+            DestroyImmediate(transform.GetChild(transform.childCount - 1).gameObject, false);
+        }
+    }
+}
