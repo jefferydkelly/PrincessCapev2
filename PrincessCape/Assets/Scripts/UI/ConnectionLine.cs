9363a2b5821dd86901560c65f87c5f53b3b3c5a1
Jeffery "J.D." Kelly <neohero22@gmail.com>
2018-07-23 14:01:25 -0700

    Change how ConnectionLines scale so that they are composed of multiple links instead of one constantly scaling element.

--- PrincessCape/Assets/Scripts/UI/ConnectionLine.cs
+++ PrincessCape/Assets/Scripts/UI/ConnectionLine.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class ConnectionLine : MonoBehaviour
 {
 
+    [SerializeField]
+    GameObject connectionLink;
     ConnectionStruct connection;
 
     public ConnectionStruct Connection
@@ -32,8 +34,35 @@ public class ConnectionLine : MonoBehaviour
         MapTile activator = connection.ActivatorTile;
         MapTile activated = connection.ActivatedTile;
         Vector3 dif = (activated.Center - activator.Center);
-        transform.position = (activator.Center + dif / 2).SetZ(-1);
-        transform.localScale = new Vector3(dif.magnitude, 1, 1);
+       //(activator.Center + dif / 2).SetZ(-1);
+        //transform.localScale = new Vector3(dif.magnitude, 1, 1);
         transform.rotation = Quaternion.AngleAxis(Mathf.Atan2(dif.y, dif.x).ToDegrees(), Vector3.forward);
+        transform.position = activator.Center.SetZ(-1);
+        SetLength(dif.magnitude);
+    }
+
+    void SetLength(float length) {
+        if (length < transform.childCount)
+        {
+            int numChildren = transform.childCount + 1;
+
+            while (length < numChildren)
+            {
+                Destroy(transform.GetChild(transform.childCount - 1).gameObject);
+                numChildren--;
+            }
+        }
+        else
+        {
+
+            while (transform.childCount + 1 < length)
+            {
+                GameObject link = Instantiate(connectionLink);
+                link.transform.SetParent(transform);
+                link.transform.localPosition = Vector3.right * transform.childCount;
+                link.transform.localRotation = Quaternion.identity;
+            }
+        }
+
     }
 }
