18e1d8e3984581c5b9cd36339937f02e066227cc
Jeffery "J.D." Kelly <neohero22@gmail.com>
2018-09-06 10:37:57 -0700

    Fixed an issue where Air Columns were still animating in the Level Editor.

--- PrincessCape/Assets/Scripts/Tiles/AirColumn.cs
+++ PrincessCape/Assets/Scripts/Tiles/AirColumn.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class AirColumn : MapTile {
 
+    Animator myAnimator;
+    public override void Init()
+    {
+        base.Init();
+        myAnimator = GetComponent<Animator>();
+        myAnimator.SetBool("Activated", Game.Instance.IsPlaying);
+    }
     private void OnTriggerStay2D(Collider2D collision)
     {
         collision.attachedRigidbody.AddForce(transform.up * 15);
